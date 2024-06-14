e0c1b442419843f2cf9e1ce16ce31b68e752a092
Coding <d.gogodze@homtail.com>
2024-06-14 16:23:56 +0400

    first ver

---
+++ Db.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class Db:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+
+    def create(self, name: str, surname: str, pasword: str, email: str):
+        query = "INSERT INTO Users (name ,surname, password, Email) VALUES (?, ?, ?, ?)"
+        self.cursor.execute(query, (name, surname, pasword, email))
+        self.conn.commit()
+
+    def read(self, email: str):
+        self.cursor.execute("SELECT * FROM users where Email = (?)", (email,))
+        return self.cursor.fetchall()
+
+    def update(self, name: str, surname: str, pasword: str, email: str):
+        self.cursor.execute("Update users set name = (?), surname = (?), password = (?) where Email = (?)", (name, surname, pasword, email))
+        self.conn.commit()
+        return self.cursor.fetchall()
+
+    def delete(self, email: str):
+        self.cursor.execute('DELETE FROM users WHERE Email = (?)', (email,))
+        self.conn.commit()
---
+++ main.py
@@ -0,0 +1,19 @@
+from Db import Db
+import Gui
+
+
+global buttonclicked
+
+
+
+def login():
+    print("clicc")
+
+
+def main():
+    app = App()
+
+if __name__ == '__main__':
+    main()
+
+
